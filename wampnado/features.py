e1a7a1c8f020931d77e3c65445f98435c706812c
estith <e.stith@baeri.org>
2019-05-30 15:57:53 -0700

    Activated some client features struct items.

--- wampnado/features.py
+++ wampnado/features.py
@@ -35,40 +35,40 @@ class Options(dict):
 
 
 # Supported client features.
-#client_features = Options(
-#    roles=Options(
-#        caller=Options(
-#            features=Options(
-#                caller_identification=True,
-#                call_canceling=True,
-#                progressive_call_results=True,
-#            )
-#        ),
-#        callee=Options(
-#            features=Options(
-#                caller_identification=True,
-#                pattern_based_registration=True,
-#                shared_registration=True,
-#                progressive_call_results=True,
-#                registration_revocation=True,
-#            )
-#        ),
-#        publisher=Options(
-#            features=Options(
-#                publisher_identification=True,
-#                subscriber_blackwhite_listing=True,
-#                publisher_exclusion=True,
-#            )
-#        ),
-#        subscriber=Options(
-#            features=Options(
-#                publisher_identification=True,
-#                pattern_based_subscription=True,
-#                subscription_revocation=True
-#            )
-#        )
-#    )
-#)
+client_features = Options(
+    roles=Options(
+        caller=Options(
+            features=Options(
+                #caller_identification=True,
+                #call_canceling=True,
+                #progressive_call_results=True,
+            )
+        ),
+        callee=Options(
+            features=Options(
+                #caller_identification=True,
+                #pattern_based_registration=True,
+                #shared_registration=True,
+                #progressive_call_results=True,
+                #registration_revocation=True,
+            )
+        ),
+        publisher=Options(
+            features=Options(
+                #publisher_identification=True,
+                #subscriber_blackwhite_listing=True,
+                #publisher_exclusion=True,
+            )
+        ),
+        subscriber=Options(
+            features=Options(
+                #publisher_identification=True,
+                #pattern_based_subscription=True,
+                #subscription_revocation=True
+            )
+        )
+    )
+)
 
 
 # Supported server features.
